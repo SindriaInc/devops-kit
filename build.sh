42fa132102f84f2b8863806997f694559589cb78
lucapitzoi <luca.pitzoi@sindria.org>
2021-06-06 17:45:51 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -13,10 +13,12 @@ fi
 IMAGE_NAME=$1
 TAG_VERSION=$2
 
+HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
     --tag ${IMAGE_NAME}:${TAG_VERSION} \
     --tag ${IMAGE_NAME}:latest \
     --build-arg TAG_VERSION=${TAG_VERSION} \
+    --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
